19994ec7ecf9fc18e5112a2acfba00e25b6ae9fe
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-09-01 12:43:40 +0530

    register user

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+// const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
+// app.use(bodyParser.urlencoded({extended:true}));
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
@@ -20,58 +22,96 @@ require("./connection/mongoose");
 const User = require("./models/user");
 
 passport.use(User.createStrategy());
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.post("/signUp", async(req, res) =>{
-    var userName = req.body.userName;
+app.post("/signUp", async (req, res) => {
+    var user_name = req.body.username;
+    var email = req.body.email;
     var password = req.body.password;
-    console.log(userName);
-    console.log(password);
-    User.register({username: userName}, password, function (err, user) {
-        if(err){
-            console.log(err);
-            res.redirect("/signUp");
-        }else{
-            passport.authenticate("local")(req, res, function(){
-                res.redirect("/home");
+    try {
+        await User.register(
+            { username: user_name, email: email },
+            password,
+            function (err, user) {
+                if (err) {
+                    console.log(err);
+                    res.redirect("/signUp");
+                } else {
+                    passport.authenticate("local")(req, res, function () {
+                        res.send(user);
+                        //res.redirect("/home");
+                    });
+                }
             });
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+app.post("/signIn", function (res, req)  {
+
+    var user_name = req.body.username;
+    var password = req.body.password;
+    console.log(user_name);
+    console.log(password);
+
+    // const user = new User({
+    //     username: req.body.username,
+    //     password: req.body.password
+    // });
+    // try {
+    //     req.login(user, function(err) {
+    //         if(err){
+    //             console.log(e);
+    //         }else{
+    //             passport.authenticate("local")(req, res, function(){
+    //                 res.send(user);
+    //                 //res.redirect("/home");
+    //             });
+    //         }
+    //     });
+    // } catch (e) {
+    //     console.log(e);
+    // }
+});
+
+app.get("/users", async (req, res) => {
+    if(req.isAuthenticated()){
+        try {
+            await res.send("get");
+            console.log("success!");
+        } catch (e) {
+            console.log(e);
         }
-    });
-});
-
-app.get("/home", async(req, res) => {
-    try{
-        await res.send("home");
-        console.log("success!");
-    } catch (e) {
-        console.log(e);
+    }else{
+        //res.redirect("/login");
     }
 });
 
-app.get("/users", async(req, res) => {
-    try{
-        await res.send("get");
-        console.log("success!");
-    } catch (e) {
-        console.log(e);
+app.patch("user/:id", async (req, res) => {
+    if(req.isAuthenticated()){
+        try {
+            await res.send("get");
+            console.log("success!");
+        } catch (e) {
+            console.log(e);
+        }
+    }else{
+        //res.redirect("/login");
     }
 });
 
-app.patch("user/:id", async(req, res) => {
-    try{
-
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-app.delete("user/:id", async(req, res) => {
-    try{
-
-    } catch (e) {
-        console.log(e);
+app.delete("user/:id", async (req, res) => {
+    if(req.isAuthenticated()){
+        try {
+            await res.send("get");
+            console.log("success!");
+        } catch (e) {
+            console.log(e);
+        }
+    }else{
+        //res.redirect("/login");
     }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,21 +2,24 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true
-    }
+    username: String,
+    email: String,
+    password: String
+    // username: {
+    //     type: String,
+    //     required: true,
+    //     trim: true
+    // },
+    // email: {
+    //     type: String,
+    //     required: true,
+    //     trim: true
+    // },
+    // password: {
+    //     type: String,
+    //     required: true,
+    //     trim: true
+    // }
 });
 
 userSchema.plugin(passportLocalMongoose);
